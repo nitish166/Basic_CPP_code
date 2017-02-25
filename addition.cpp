988a679069a0786189b7afda01d83957a07da6a4
Nitish <knitish123@gmail.com>
2017-02-25 20:39:19 +0530

    addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,16 @@
+/* Additon of two number program */
+// date 4 jan time:- 10:43
+// Nitish kumar
+
+#include<iostream>   // input and output handle in stream of byte    
+using namespace std;
+int main()
+{
+	int A,B,C;  //varable declaration
+	cout<<"Enter two integer varable :";  //display on console to request user to input number
+	cin>>A>>B;   // input two integer value by user
+	C=A+B; //arthematic operation adding of value of A and B and store in varable C
+	cout<<"Addtion of two number is :"<<C;
+	return 0;  // back to operating system means call back to main function
+
+}
